b4d2066f6142734e9ae10a38aed63b1de568ab9b
Volper212 <volpergaming@gmail.com>
2021-12-23 19:39:10 +0100

    Add routing

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 app.use(express.static("frontend/public"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve("frontend/public/index.html"));
+});
+
 app.listen(process.env.PORT || 3000);
