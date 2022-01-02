2d32fa62fe43ef8130224066c9661e37fba5c6f4
szyplc <szyplokarz@gmail.com>
2022-01-02 01:12:01 +0100

    Added list and connetion to database

---
+++ frontend/components/List.svelte
@@ -0,0 +1,21 @@
+<script type="ts">
+
+
+    (async () => {
+        const rawResponse = await fetch("/test", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        const content = await rawResponse.json();
+        console.log(content);
+    })();
+</script>
+
+Elo
+
+<style>
+
+</style>
