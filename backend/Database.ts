0108b88035197e2da6969e3c9c06835f7fcbd31c
c3zario <62115336+c3zario@users.noreply.github.com>
2022-01-03 02:44:34 +0100

    Authorization - register, login

--- backend/Database.ts
+++ backend/Database.ts
@@ -1,21 +1,18 @@
-import * as mongoDB from "mongodb";
-import * as dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
-let process = {
-    env: {DB_CONN_STRING: "mongodb://localhost:27017",
-    DB_NAME: "HomeOn",
-    COLLECTION_NAME_list: "List"}
-}
-export const collections: { list?: mongoDB.Collection} = {}
-export let List = {};
-export async function connectToDatabase() {
-    dotenv.config();
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
-    await client.connect();
-    const db: mongoDB.Db = client.db(process.env.DB_NAME);
-    const Collection_list: mongoDB.Collection = db.collection(process.env.COLLECTION_NAME_list);
-    collections.list = Collection_list;
-    //Pobranie listy z kolekcji
-    List = await collections?.list?.find({}).toArray();
+let env = {
+    DB_CONN: "mongodb://localhost:27017/",
+    DB_NAME: "HomeON"
 }
 
+const conn = MongoClient.connect(env.DB_CONN)
+
+export async function Insert(collect: string, obj: any) {
+    const collection = (await conn).db(env.DB_NAME).collection(collect)
+    await collection.insertOne(obj)
+}
+
+export async function Find(collect: string, obj: any) {
+    const collection = (await conn).db(env.DB_NAME).collection(collect)
+    return await collection.find(obj).toArray()
+}
