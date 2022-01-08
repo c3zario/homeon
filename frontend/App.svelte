419c7abd52f6b7c0223691f3f2b35109feae608f
Volper212 <volpergaming@gmail.com>
2022-01-08 19:04:51 +0100

    Refactor Czaros

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,169 +1,5 @@
-<style lang="scss">
-    @import "./styles/variables.scss";
-
-    :global(*, ::before, ::after) {
-        box-sizing: border-box;
-    }
-
-    #home {
-        height: 100%;
-        display: flex;
-        flex-flow: column;
-
-        #user_profile {
-            display: flex;
-            flex-flow: row;
-            padding: 4vmin;
-
-            background-color: $p-color-dark;
-
-            > div {
-                flex: 1;
-
-                &:nth-child(1) {
-                    display: flex;
-                    
-                    > div {
-                        display: flex;
-                        flex-flow: column;
-                        align-items: center;
-                        justify-content: left;
-
-                        font-variant: small-caps;
-                        color: white;
-
-                        button {
-                            border: none;
-                            border-radius: 2vmin;
-                            
-                            padding: 3vmin 7.5vmin;
-                            margin: 1.5vmin;
-
-                            box-shadow: -0.5vmin -0.5vmin 1vmin 0vmin #00000040, 0.5vmin 0.5vmin 1vmin 0vmin #00000040;
-                            
-                            background-color: $s-color-dark;
-                            color: white;
-                            font-size: 3vmin;
-                            text-transform: lowercase;
-                            font-variant: small-caps;
-                        }
-                    }
-                }
-
-                &:nth-child(2) {
-                    display: flex;
-                    justify-content: right;
-
-                    #home {
-                        width: 10vmin;
-                        background-repeat: no-repeat;
-                        background-position: center;
-                        background-image: url('icons/home.png');
-                        background-size: 90%;
-                    }
-                }
-            }
-        }
-
-        #info_block {
-            flex: 1;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-
-            > div {
-                flex: 1;
-                display: flex;
-                flex-flow: column;
-
-                height: 40vmin;
-                > div {
-                    flex: 1;
-                    margin: 1vmin 4vmin 1vmin;
-
-                    border-radius: 2vmin;
-                    background-color: $p-color;
-                }
-            }
-        }
-
-        #navigation {
-            flex: 2.5;
-            display: flex;
-            align-items: center;
-
-            > div {
-                flex: 1;
-                display: flex;
-                flex-flow: column;
-
-                > div {
-                    flex: 1;
-                    
-                    display: flex;
-                    flex-flow: row;
-                    align-items: center;
-                    justify-content: center;
-
-                    padding-top: 4vmin;
-                    padding-bottom: 4vmin;
-
-                    > div {
-                        height: 35vmin;
-                        width: 90vmin;
-
-                        display: flex;
-                        flex-flow: row;
-                        justify-content: center;
-                        
-                        > a {
-                            display: contents;
-
-                            > div {
-                                flex: 1;
-                                background-color: $p-color;
-                                border-radius: 2vmin;
-
-                                background-repeat: no-repeat;
-                                background-position: center;
-                            }
-                        }
-
-                        #home_menage {
-                            margin-left: 2.5vmin;
-                            margin-right: 4vmin;
-                            background-image: url('icons/home_menage.png');
-                            background-size: 40%;
-                        }
-
-                        #plans {
-                            margin-left: 4vmin;
-                            margin-right: 2.5vmin;
-                            background-image: url('icons/plans.png');
-                            background-size: 40%;
-                        }
-
-                        #shopping_list {
-                            margin-left: 2.5vmin;
-                            margin-right: 2.5vmin;
-                            background-image: url('icons/shopping_list.png');
-                            background-size: 18%;
-                        }
-
-                        #household_duties {
-                            margin-left: 2.5vmin;
-                            margin-right: 2.5vmin;
-                            background-image: url('icons/clean.png');
-                            background-size: 18%;
-                        }
-                    }
-                }
-            }
-        }
-    }
-</style>
 <script type="ts">
-    import { Router, Link, Route } from "svelte-routing";
+    import { Router, link, Route } from "svelte-routing";
 
     import Authorization from "./components/Authorization.svelte";
     import List from "./components/list.svelte";
@@ -176,8 +12,8 @@
         <Authorization />
     {:else}
         <Router>
-            <div id="home">
-                <div id="user_profile">
+            <div class="home">
+                <div class="user-profile">
                     <div>
                         <div>
                             {user.login}
@@ -190,33 +26,41 @@
                         </div>
                     </div>
                     <div>
-                        <a href="/"><div id="home"></div></a>
+                        <a href="/" use:link><div class="home" /></a>
                     </div>
                 </div>
                 <Route path="/">
-                    <div id="info_block">
+                    <div class="info-block">
                         <div>
-                            <div></div>
-                            <div></div>
+                            <div />
+                            <div />
                         </div>
                     </div>
-                
-                    <div id="navigation">
+
+                    <div class="navigation">
                         <div>
                             <div>
                                 <div>
-                                    <a href="/home_menage"><div id="home_menage"></div></a>
-                                    <a href="/plans"><div id="plans"></div></a>
+                                    <a href="/home-manage" use:link>
+                                        <div class="home-manage" />
+                                    </a>
+                                    <a href="/plans" use:link>
+                                        <div class="plans" />
+                                    </a>
                                 </div>
                             </div>
                             <div>
                                 <div>
-                                    <a href="/shopping_list"><div id="shopping_list"></div></a>
+                                    <a href="/shopping-list" use:link>
+                                        <div class="shopping-list" />
+                                    </a>
                                 </div>
                             </div>
                             <div>
                                 <div>
-                                    <a href="/household_duties"><div id="household_duties"></div></a>
+                                    <a href="/household-duties" use:link>
+                                        <div class="household-duties" />
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -240,3 +84,169 @@
         </Router>
     {/if}
 {/await}
+
+<style lang="scss">
+    @import "./styles/variables.scss";
+
+    :global(*, ::before, ::after) {
+        box-sizing: border-box;
+    }
+
+    .home {
+        height: 100%;
+        display: flex;
+        flex-flow: column;
+
+        .user-profile {
+            display: flex;
+            flex-flow: row;
+            padding: 4vmin;
+
+            background-color: $p-color-dark;
+
+            > div {
+                flex: 1;
+
+                &:nth-child(1) {
+                    display: flex;
+
+                    > div {
+                        display: flex;
+                        flex-flow: column;
+                        align-items: center;
+                        justify-content: left;
+
+                        font-variant: small-caps;
+                        color: white;
+
+                        button {
+                            border: none;
+                            border-radius: 2vmin;
+
+                            padding: 3vmin 7.5vmin;
+                            margin: 1.5vmin;
+
+                            box-shadow: -0.5vmin -0.5vmin 1vmin 0vmin #00000040,
+                                0.5vmin 0.5vmin 1vmin 0vmin #00000040;
+
+                            background-color: $s-color-dark;
+                            color: white;
+                            font-size: 3vmin;
+                            text-transform: lowercase;
+                            font-variant: small-caps;
+                        }
+                    }
+                }
+
+                &:nth-child(2) {
+                    display: flex;
+                    justify-content: right;
+
+                    .home {
+                        width: 10vmin;
+                        background-repeat: no-repeat;
+                        background-position: center;
+                        background-image: url("icons/home.png");
+                        background-size: 90%;
+                    }
+                }
+            }
+        }
+
+        .info-block {
+            flex: 1;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            > div {
+                flex: 1;
+                display: flex;
+                flex-flow: column;
+
+                height: 40vmin;
+                > div {
+                    flex: 1;
+                    margin: 1vmin 4vmin 1vmin;
+
+                    border-radius: 2vmin;
+                    background-color: $p-color;
+                }
+            }
+        }
+
+        .navigation {
+            flex: 2.5;
+            display: flex;
+            align-items: center;
+
+            > div {
+                flex: 1;
+                display: flex;
+                flex-flow: column;
+
+                > div {
+                    flex: 1;
+
+                    display: flex;
+                    flex-flow: row;
+                    align-items: center;
+                    justify-content: center;
+
+                    padding-top: 4vmin;
+                    padding-bottom: 4vmin;
+
+                    > div {
+                        height: 35vmin;
+                        width: 90vmin;
+
+                        display: flex;
+                        flex-flow: row;
+                        justify-content: center;
+
+                        > a {
+                            display: contents;
+
+                            > div {
+                                flex: 1;
+                                background-color: $p-color;
+                                border-radius: 2vmin;
+
+                                background-repeat: no-repeat;
+                                background-position: center;
+                            }
+                        }
+
+                        .home-manage {
+                            margin-left: 2.5vmin;
+                            margin-right: 4vmin;
+                            background-image: url("icons/home-manage.png");
+                            background-size: 40%;
+                        }
+
+                        .plans {
+                            margin-left: 4vmin;
+                            margin-right: 2.5vmin;
+                            background-image: url("icons/plans.png");
+                            background-size: 40%;
+                        }
+
+                        .shopping-list {
+                            margin-left: 2.5vmin;
+                            margin-right: 2.5vmin;
+                            background-image: url("icons/shopping-list.png");
+                            background-size: 18%;
+                        }
+
+                        .household-duties {
+                            margin-left: 2.5vmin;
+                            margin-right: 2.5vmin;
+                            background-image: url("icons/clean.png");
+                            background-size: 18%;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
