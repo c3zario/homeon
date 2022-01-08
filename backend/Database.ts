c28b82f1b8585e00dca094b53bfbe9d0ab4da81f
Volper212 <volpergaming@gmail.com>
2022-01-08 14:43:19 +0100

    Refactor database

--- backend/Database.ts
+++ backend/Database.ts
@@ -1,42 +1,22 @@
-import * as mongoDB from "mongodb";
+import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 
 let process = {
     env: {
         DB_CONN_STRING: "mongodb://localhost:27017",
         DB_NAME: "HomeON",
-        COLLECTION_NAME_list: "List",
-        COLLECTION_NAME_users: "users",
-        COLLECTION_NAME_groups: "groups",
     },
 };
-export const collections: {
-    list?: mongoDB.Collection;
-    users?: mongoDB.Collection;
-    groups?: mongoDB.Collection;
-    plans?: mongoDB.Collection;
-} = {};
-export let List = {};
-export async function connectToDatabase() {
-    try {
-        dotenv.config();
-        const client: mongoDB.MongoClient = new mongoDB.MongoClient(
-            process.env.DB_CONN_STRING
-        );
-        await client.connect();
-        const db: mongoDB.Db = client.db(process.env.DB_NAME);
-        const Collection_list: mongoDB.Collection = db.collection(
-            process.env.COLLECTION_NAME_list
-        );
-        collections.list = Collection_list;
-        const Collection_users: mongoDB.Collection = db.collection(
-            process.env.COLLECTION_NAME_users
-        );
-        collections.users = Collection_users;
-        collections.groups = db.collection(process.env.COLLECTION_NAME_groups);
-        //Pobranie listy z kolekcji
-        List = await collections?.list?.find({}).toArray();
-    } catch (err) {
-        console.log(err);
-    }
+
+export async function getDatabase() {
+    dotenv.config();
+    const client = new MongoClient(process.env.DB_CONN_STRING);
+    await client.connect();
+    const database = client.db(process.env.DB_NAME);
+    return {
+        list: database.collection("list"),
+        users: database.collection("users"),
+        groups: database.collection("groups"),
+        plans: database.collection("plans"),
+    };
 }
--- backend/app.ts
+++ backend/app.ts
@@ -1,58 +1,80 @@
 import express from "express";
 import path from "path";
-import { connectToDatabase, List, collections } from "./Database";
+import { getDatabase } from "./Database";
 import bodyParser from "body-parser";
 import bcrypt from "bcryptjs";
 import { nanoid } from "nanoid";
 import cookieSession from "cookie-session";
 
-const app = express();
-app.use(express.static("frontend/public"));
-app.use(bodyParser.text());
-app.use(express.json());
-app.use(
-    cookieSession({
-        name: "session",
-        keys: ["a"],
-    })
-);
+main();
 
-Main();
-async function Main() {
-    await connectToDatabase();
+async function main() {
+    const app = express();
+    app.use(express.static("frontend/public"));
+    app.use(bodyParser.text());
+    app.use(express.json());
+    app.use(
+        cookieSession({
+            name: "session",
+            keys: ["a"],
+        })
+    );
+    const database = await getDatabase();
+
+    app.get("/api/checkLogin", (req: any, res) => {
+        res.send(req.session.user ? req.session.user : {});
+    });
+
+    // authorization
+    app.post("/register", async (req: any, res) => {
+        res.send(await register(req));
+    });
+
+    app.post("/login", async (req: any, res) => {
+        res.send(await login(req));
+    });
+
+    app.post("/confirm", async (req: any, res) => {
+        res.send(await confirm(req));
+    });
+
+    app.get("/logout", async (req: any, res) => {
+        req.session.user = {};
+        res.send();
+    });
 
     app.post("/api/add-group", (req, res) => {
         const token = nanoid();
-        collections.groups?.insertOne({
+        database.groups.insertOne({
             name: req.body.name,
             token,
             plans: [],
         });
-        collections.users?.updateOne(
+        database.users.updateOne(
             { login: req.session?.user.login },
             { $push: { groups: token } }
         );
     });
 
     app.get("/api/groups", async (req, res) => {
-        const user = await collections.users?.findOne({
+        const user = await database.users.findOne({
             login: req.session?.user.login,
         });
-        const groups = await collections.groups
-            ?.find({ token: { $in: user?.groups } })
+        const groups = await database.groups
+            .find({ token: { $in: user?.groups } })
             .toArray();
         res.send(groups);
     });
 
     app.post("/api/add-plan", (req, res) => {
-        collections.groups?.updateOne(
+        database.groups.updateOne(
             { token: req.body.token },
             { $push: { plans: req.body.plan } }
         );
     });
 
     app.get("/group/:token", (req, res) => {
-        collections.users?.updateOne(
+        database.users.updateOne(
             { login: req.session?.user.login },
             { $push: { groups: req.params.token } }
         );
@@ -64,123 +86,103 @@ async function Main() {
     });
 
     app.post("/test", async (req, res) => {
-        let list = await collections?.list?.find({}).toArray();
+        let list = await database.list.find({}).toArray();
         res.send(list);
     });
     app.post("/updateList", (req, res) => {
         //error
-        collections.list?.drop();
+        database.list.drop();
         if (JSON.parse(req.body).length > 0)
-            collections.list?.insertMany(JSON.parse(req.body));
+            database.list.insertMany(JSON.parse(req.body));
     });
     app.post("/addToList", async (req, res) => {
         //console.log(req.body);
-        let last: any = await collections.list
-            ?.find({})
+        let last: any = await database.list
+            .find({})
             .sort({ _id: -1 })
             .limit(1)
             .toArray();
         last = last[0] ? last[0].id : 0;
         let d = JSON.parse(req.body);
         let obj = { id: last + 1, text: d.text, count: d.count };
-        collections.list?.insertOne(obj);
+        database.list.insertOne(obj);
     });
 
     app.listen(process.env.PORT || 3000);
-}
 
-app.get("/api/checkLogin", (req: any, res) => {
-    res.send(req.session.user ? req.session.user : {});
-});
+    async function register(req: any) {
+        const email = await database.users.findOne({ email: req.body.email });
+        if (email) return "Podany adres email został już zarejestrowany";
 
-// authorization
-app.post("/register", async (req: any, res) => {
-    res.send(await register(req));
-});
+        const login = await database.users.findOne({ login: req.body.login });
+        if (login) return "Podany login jest już zajęty";
 
-app.post("/login", async (req: any, res) => {
-    res.send(await login(req));
-});
+        const personalGroupToken = nanoid();
+        const confirmEmailToken = "r" + nanoid();
 
-app.post("/confirm", async (req: any, res) => {
-    res.send(await confirm(req));
-});
+        await Promise.all([
+            database.users.insertOne({
+                login: req.body.login,
+                password: await bcrypt.hash(req.body.password, 10),
+                email: req.body.email,
+                confirmEmailToken,
+                groups: [personalGroupToken],
+            }),
+            database.groups.insertOne({
+                name: "Osobiste",
+                token: personalGroupToken,
+                plans: [],
+            }),
+        ]);
 
-app.get("/logout", async (req: any, res) => {
-    req.session.user = {};
-    res.send();
-});
+        console.log(confirmEmailToken);
+        return "Konto zostało utworzone! Dokończ rejestrację klikając w link wysłany na podany adres email";
+    }
 
-async function register(req: any) {
-    const [email]: any = await collections.users
-        ?.find({ email: req.body.email })
-        .toArray();
-    if (email) return "Podany adres email został już zarejestrowany";
-
-    const [login]: any = await collections.users
-        ?.find({ login: req.body.login })
-        .toArray();
-    if (login) return "Podany login jest już zajęty";
-
-    const personalGroupToken = nanoid();
-    const confirmEmailToken = "r" + nanoid();
-    await Promise.all([
-        collections.users?.insertOne({
-            login: req.body.login,
-            password: await bcrypt.hash(req.body.password, 10),
-            email: req.body.email,
+    async function confirm(req: any) {
+        const confirmEmailToken = req.body.confirmEmailToken;
+        const user: any = await database.users.findOne({
             confirmEmailToken,
-            groups: [personalGroupToken],
-        }),
-        collections.groups?.insertOne({
-            name: "Osobiste",
-            token: personalGroupToken,
-            plans: [],
-        }),
-    ]);
+        });
 
-    console.log(confirmEmailToken);
-    return "Konto zostało utworzone! Dokończ rejestrację klikając w link wysłany na podany adres email";
-}
+        if (user) {
+            await database.users.updateOne(
+                { confirmEmailToken },
+                { $unset: { confirmEmailToken: 1 } }
+            );
 
-async function confirm(req: any) {
-    const confirmEmailToken = req.body.confirmEmailToken;
-    const user: any = await collections.users?.findOne({ confirmEmailToken });
+            req.session.user = {
+                login: user.login,
+                email: user.email,
+            };
 
-    if (user) {
-        await collections.users?.updateOne(
-            { confirmEmailToken },
-            { $unset: { confirmEmailToken: 1 } }
+            return true;
+        }
+
+        return false;
+    }
+
+    async function login(req: any) {
+        const user = await database.users.findOne({
+            login: req.body.login,
+        });
+        if (!user) return "Konto o podanym loginie nie istnieje";
+
+        const passwordHash = await bcrypt.compare(
+            req.body.password,
+            user.password
         );
+
+        if (!passwordHash) return "Niepoprawne hasło";
+
+        if (user.confirmEmailToken)
+            return "Dokończ rejestrację klikając w link wysłany na podany adres email";
 
         req.session.user = {
             login: user.login,
             email: user.email,
         };
 
-        return true;
+        return "";
     }
-
-    return false;
-}
-
-async function login(req: any) {
-    const user: any = await collections.users?.findOne({
-        login: req.body.login,
-    });
-    if (!user) return "Konto o podanym loginie nie istnieje";
-
-    const passwordHash = await bcrypt.compare(req.body.password, user.password);
-
-    if (!passwordHash) return "Niepoprawne hasło";
-
-    if (user.confirmEmailToken)
-        return "Dokończ rejestrację klikając w link wysłany na podany adres email";
-
-    req.session.user = {
-        login: user.login,
-        email: user.email,
-    };
-
-    return "";
 }
