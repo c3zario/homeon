e8b7168a800ee93bca3f6e1ca1906d6a0afc0a2a
Volper212 <volpergaming@gmail.com>
2022-03-03 20:44:40 +0100

    Fix HomeManage

--- frontend/pages/HomeManage.svelte
+++ frontend/pages/HomeManage.svelte
@@ -1,24 +1,23 @@
 <script type="ts">
-    let lights: any = {0: {}}
+    let lights: any = { 0: {} };
     fetch("/get-lights", {
         method: "POST",
     })
-    .then( odp => odp.json() )
-    .then( v => {
-        lights = v
+        .then((odp) => odp.json())
+        .then((v) => {
+            lights = v;
 
-        Object.values(lights).forEach((light, key) => {
-            name[key] = {
-                name: light.name,
-                work: light.work
-            }
-        })
-    })
+            Object.values(lights).forEach((light: any, key) => {
+                name[key] = {
+                    name: light.name,
+                    work: light.work,
+                };
+            });
+        });
 
-
-    let search = false    
+    let search = false;
     function addLight() {
-        search = true
+        search = true;
         fetch("/add-light", {
             method: "POST",
         });
@@ -27,30 +26,30 @@
             fetch("/check-add-light", {
                 method: "POST",
             })
-            .then( odp => odp.json() )
-            .then( v => {
-                if(v) {
-                    console.log("get")
-                    clearInterval(checkLight)
+                .then((odp) => odp.json())
+                .then((v) => {
+                    if (v) {
+                        console.log("get");
+                        clearInterval(checkLight);
 
-                    fetch("/get-lights", {
-                        method: "POST",
-                    })
-                    .then( odp => odp.json() )
-                    .then( v => {
-                        lights = v
-                    })
-                    
-                    search = false
-                }
-            })
-        }, 2000)
+                        fetch("/get-lights", {
+                            method: "POST",
+                        })
+                            .then((odp) => odp.json())
+                            .then((v) => {
+                                lights = v;
+                            });
+
+                        search = false;
+                    }
+                });
+        }, 2000);
     }
 
-    let editLightSwitch = false
+    let editLightSwitch = false;
 
-    let name: any = [Object.values(lights).length]
-    
+    let name: any = [Object.values(lights).length];
+
     function editLight() {
         fetch("/edit-lights", {
             method: "POST",
@@ -62,46 +61,67 @@
 <div id="all">
     <div>
         {#if !editLightSwitch}
-            <div class="edit" on:click="{() => {editLightSwitch = !editLightSwitch}}"><i class="icon-edit" /></div>
+            <div
+                class="edit"
+                on:click={() => {
+                    editLightSwitch = !editLightSwitch;
+                }}
+            >
+                <i class="icon-edit" />
+            </div>
         {:else}
-            <div class="edit" on:click="{() => {editLightSwitch = !editLightSwitch; editLight()}}"><i class="icon-v" /></div>
+            <div
+                class="edit"
+                on:click={() => {
+                    editLightSwitch = !editLightSwitch;
+                    editLight();
+                }}
+            >
+                <i class="icon-v" />
+            </div>
         {/if}
         {#each Object.values(lights) as light, key}
-        <div class="light">
-            <div class="title">
+            <div class="light">
+                <div class="title">
+                    {#if !editLightSwitch}
+                        {name[key].name}
+                    {:else}
+                        <input type="text" bind:value={name[key].name} />
+                    {/if}
+                </div>
                 {#if !editLightSwitch}
-                    {name[key].name}
-                {:else}
-                    <input type="text" bind:value={name[key].name}>
+                    <div class="light_switch">
+                        <input
+                            type="checkbox"
+                            id="switch{key}"
+                            class="switch"
+                            bind:checked={name[key].work}
+                            on:change={editLight}
+                        />
+                        <label for="switch{key}" class="lbl-off">Off</label>
+                        <label for="switch{key}" class="lbl-on">On</label>
+                    </div>
                 {/if}
             </div>
-            {#if !editLightSwitch}
-                <div class="light_switch">
-                    <input type="checkbox" id="switch{key}" class="switch" bind:checked={name[key].work} on:change="{editLight}"/>
-                    <label for="switch{key}" class="lbl-off">Off</label>
-                    <label for="switch{key}" class="lbl-on">On</label>
-                </div>
-            {/if}
-        </div>
         {/each}
     </div>
     {#if search}
-    <div id="load"><i class="icon-load animate-spin" /></div>
+        <div id="load"><i class="icon-load animate-spin" /></div>
     {:else}
-    <div id="add" on:click="{addLight}">
-        <div><i class="icon-plus" /></div>
-    </div>
+        <div id="add" on:click={addLight}>
+            <div><i class="icon-plus" /></div>
+        </div>
     {/if}
 </div>
 
 <style lang="scss">
+    @use "sass:math";
     @import "../styles/variables.scss";
 
-    $height: 30 / 16 * 1em;
-    $width: 72 / 16 * 1em;
+    $height: math.div(30, 16) * 1em;
+    $width: math.div(72, 16) * 1em;
 
-    $transition-default: 0.25s ease-out 0.10s;
-
+    $transition-default: 0.25s ease-out 0.1s;
 
     #all {
         margin-top: 15vmin;
@@ -110,7 +130,7 @@
         flex-flow: column;
         align-items: center;
         justify-content: center;
-        
+
         > div {
             display: flex;
             flex-flow: column;
@@ -122,11 +142,11 @@
                 left: 2vmin;
                 background-color: $s-color;
                 color: white;
-                
+
                 padding: 2vmin;
                 border-radius: 15vmin;
             }
-            
+
             .light {
                 display: flex;
                 flex-flow: row;
@@ -140,7 +160,7 @@
                 .title {
                     border-radius: 2vmin;
                     padding: 3vmin 6vmin;
-                    
+
                     background-color: $p-color;
                     color: white;
 
@@ -162,19 +182,19 @@
                         font-weight: bold;
                         line-height: 1em;
                         position: absolute;
-                        top: 8 / 16 * 1em;
+                        top: math.div(8, 16) * 1em;
                         transition: opacity $transition-default;
                         text-transform: uppercase;
                     }
 
                     .lbl-off {
-                        right: 7 / 16 * 1em;
+                        right: math.div(7, 16) * 1em;
                     }
 
                     .lbl-on {
                         color: white;
                         opacity: 0;
-                        left: 7 / 16 * 1em;
+                        left: math.div(7, 16) * 1em;
                     }
 
                     .switch {
@@ -193,55 +213,55 @@
 
                         &:before,
                         &:after {
-                        content: '';
-                        font-size: 1em;
-                        position: absolute;
+                            content: "";
+                            font-size: 1em;
+                            position: absolute;
                         }
 
                         &:before {
-                        border-radius: 20 / 16 * 1em;
-                        background: $s-color-light;
-                        height: $height;
-                        left: 4 / 16 * -1em;
-                        top: 3 / 16 * -1em;
-                        transition: background-color $transition-default;
-                        width: $width;
+                            border-radius: math.div(20, 16) * 1em;
+                            background: $s-color-light;
+                            height: $height;
+                            left: math.div(4, 16) * -1em;
+                            top: math.div(3, 16) * -1em;
+                            transition: background-color $transition-default;
+                            width: $width;
                         }
 
                         &:after {
-                        box-shadow: 0 .0625em .375em 0 #666;
-                        border-radius: 50%;
-                        background: white;
-                        height: 24 / 16 * 1em;
-                        transform: translate(0, 0);
-                        transition: transform $transition-default;
-                        width: 24 / 16 * 1em;
+                            box-shadow: 0 0.0625em 0.375em 0 #666;
+                            border-radius: 50%;
+                            background: white;
+                            height: math.div(24, 16) * 1em;
+                            transform: translate(0, 0);
+                            transition: transform $transition-default;
+                            width: math.div(24, 16) * 1em;
                         }
 
                         &:checked {
-                        &:after {
-                            transform: translate(40 / 16 * 1em, 0);
-                        }
+                            &:after {
+                                transform: translate(math.div(40, 16) * 1em, 0);
+                            }
 
-                        & ~ .lbl-off {
-                            opacity: 0;
-                        }
+                            & ~ .lbl-off {
+                                opacity: 0;
+                            }
 
-                        & ~ .lbl-on {
-                            opacity: 1;
-                        }
+                            & ~ .lbl-on {
+                                opacity: 1;
+                            }
                         }
 
                         &.switch {
-                        &:checked:before {
-                            background: $p-color;
-                        }
+                            &:checked:before {
+                                background: $p-color;
+                            }
                         }
                     }
-                    }
+                }
             }
         }
-        
+
         #add {
             margin-top: 5vmin;
             padding: 4vmin;
