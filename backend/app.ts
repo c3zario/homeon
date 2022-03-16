df96f3ee25dd1daf253a8e9fbe3bba89971160f5
Volper212 <swiesigor@gmai.com>
2022-03-16 14:49:51 +0100

    Fix app crashing on async error

--- backend/app.ts
+++ backend/app.ts
@@ -21,40 +21,48 @@ async function main() {
 
     io.on("connection", (socket) => {
         socket.on("newGroup", (newGroup) => {
-            socket.rooms.forEach((room) => {
-                socket.leave(room);
+            handleErrors(async () => {
+                socket.rooms.forEach((room) => {
+                    socket.leave(room);
+                });
+                socket.join(newGroup?.token);
             });
-            socket.join(newGroup?.token);
         });
-        socket.on("newPosition", async (obj) => {
-            let url = `https://api.opencagedata.com/geocode/v1/json?q=${obj.position.x}+${obj.position.y}&key=6f9855f0d9d04be3b7d02f810a09b3ca`;
-            let street = (await axios.get(url)).data.results[0].formatted;
-            await database.users.updateOne(
-                { login: obj.user.login, email: obj.user.email },
-                {
-                    $set: {
-                        position: obj.position,
-                        time: obj.time,
-                        street: street,
-                    },
-                }
-            );
-            io.to(obj.group.token).emit(
-                "position",
-                await getPositions(obj.group.token)
-            );
+        socket.on("newPosition", (obj) => {
+            handleErrors(async () => {
+                let url = `https://api.opencagedata.com/geocode/v1/json?q=${obj.position.x}+${obj.position.y}&key=6f9855f0d9d04be3b7d02f810a09b3ca`;
+                let street = (await axios.get(url)).data.results[0].formatted;
+                await database.users.updateOne(
+                    { login: obj.user.login, email: obj.user.email },
+                    {
+                        $set: {
+                            position: obj.position,
+                            time: obj.time,
+                            street: street,
+                        },
+                    }
+                );
+                io.to(obj.group.token).emit(
+                    "position",
+                    await getPositions(obj.group.token)
+                );
+            });
         });
-        socket.on("getPositions", async (group) => {
-            io.to(group.token).emit(
-                "position",
-                await getPositions(group.token)
-            );
+        socket.on("getPositions", (group) => {
+            handleErrors(async () => {
+                io.to(group.token).emit(
+                    "position",
+                    await getPositions(group.token)
+                );
+            });
         });
-        socket.on("setHome", async (obj) => {
-            await database.groups.updateOne(
-                { token: obj.group.token },
-                { $set: { home: { x: obj.lat, y: obj.lng } } }
-            );
+        socket.on("setHome", (obj) => {
+            handleErrors(async () => {
+                await database.groups.updateOne(
+                    { token: obj.group.token },
+                    { $set: { home: { x: obj.lat, y: obj.lng } } }
+                );
+            });
         });
     });
     async function getPositions(token: string) {
@@ -107,20 +115,26 @@ async function main() {
     });
 
     // authorization
-    app.post("/register", async (req, res) => {
-        res.send(await register(req));
+    app.post("/register", (req, res) => {
+        handleErrors(async () => {
+            res.send(await register(req));
+        });
     });
 
-    app.post("/login", async (req, res) => {
-        res.send(await login(req));
+    app.post("/login", (req, res) => {
+        handleErrors(async () => {
+            res.send(await login(req));
+        });
     });
 
-    app.get("/confirm/:token", async (req, res, next) => {
-        await confirm(req);
-        next();
+    app.get("/confirm/:token", (req, res, next) => {
+        handleErrors(async () => {
+            await confirm(req);
+            next();
+        });
     });
 
-    app.get("/logout", async (req, res) => {
+    app.get("/logout", (req, res) => {
         if (req.session) {
             req.session.user = {};
         }
@@ -128,153 +142,186 @@ async function main() {
     });
 
     app.post("/api/add-group", (req, res) => {
-        const token = nanoid();
-        database.groups.insertOne({
-            name: req.body.name,
-            token,
-            plans: [],
-            list: [],
-            home: null,
-            lights: [],
+        handleErrors(async () => {
+            const token = nanoid();
+            await Promise.all([
+                database.groups.insertOne({
+                    name: req.body.name,
+                    token,
+                    plans: [],
+                    list: [],
+                    home: null,
+                    lights: [],
+                }),
+                database.users.updateOne(
+                    { login: req.session?.user.login },
+                    { $push: { groups: token } }
+                ),
+            ]);
+            res.send(token);
         });
-        database.users.updateOne(
-            { login: req.session?.user.login },
-            { $push: { groups: token } }
-        );
-        res.send(token);
     });
 
-    app.post("/api/leave-group", async (req, res) => {
-        await database.users.updateOne(
-            { login: req.session?.user.login },
-            { $pull: { groups: req.body.token } }
-        );
-        const user = await database.users.findOne({
-            groups: req.body.token,
+    app.post("/api/leave-group", (req, res) => {
+        handleErrors(async () => {
+            await database.users.updateOne(
+                { login: req.session?.user.login },
+                { $pull: { groups: req.body.token } }
+            );
+            const user = await database.users.findOne({
+                groups: req.body.token,
+            });
+            if (!user) {
+                await database.groups.deleteOne({ token: req.body.token });
+            }
+            res.send();
         });
-        if (!user) {
-            await database.groups.deleteOne({ token: req.body.token });
-        }
-        res.send();
     });
 
-    app.get("/api/groups", async (req, res) => {
-        const user = await database.users.findOne({
-            login: req.session?.user.login,
+    app.get("/api/groups", (req, res) => {
+        handleErrors(async () => {
+            const user = await database.users.findOne({
+                login: req.session?.user.login,
+            });
+            const groups = await database.groups
+                .find({ token: { $in: user?.groups } })
+                .toArray();
+            res.send(groups);
         });
-        const groups = await database.groups
-            .find({ token: { $in: user?.groups } })
-            .toArray();
-        res.send(groups);
     });
 
     app.post("/api/add-plan", (req, res) => {
-        database.groups.updateOne(
-            { token: req.body.token },
-            { $push: { plans: req.body.plan } }
-        );
-        res.send();
+        handleErrors(async () => {
+            await database.groups.updateOne(
+                { token: req.body.token },
+                { $push: { plans: req.body.plan } }
+            );
+            res.send();
+        });
     });
 
     app.get("/group/:token", (req, res, next) => {
-        database.users.updateOne(
-            { login: req.session?.user.login },
-            { $push: { groups: req.params.token } }
-        );
-        next();
+        handleErrors(async () => {
+            await database.users.updateOne(
+                { login: req.session?.user.login },
+                { $push: { groups: req.params.token } }
+            );
+            next();
+        });
     });
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve("frontend/public/index.html"));
     });
 
-    app.post("/shopping-list", async (req, res) => {
-        let group = await database.groups.findOne({ token: req.body.token });
-        res.send(group?.list);
-    });
-
-    app.post("/updateList", async (req, res) => {
-        let group = JSON.parse(req.body);
-        if (JSON.parse(req.body).length > 0)
-            await database.groups.updateOne(
-                { token: group.token },
-                { $set: { list: group.list } }
-            );
-        updateRooms(group.token);
-
-        res.send();
-    });
-
-    app.post("/api/remove-plan", async (req, res) => {
-        let group = await database.groups.findOne({ token: req.body[0] });
-        let plans = group?.plans;
-        let newPlans: any = [];
-        plans?.forEach((plan) => {
-            if (plan.text != req.body[1].text) newPlans.push(plan);
+    app.post("/shopping-list", (req, res) => {
+        handleErrors(async () => {
+            let group = await database.groups.findOne({
+                token: req.body.token,
+            });
+            res.send(group?.list);
         });
-        database.groups.updateOne(
-            { token: req.body[0] },
-            { $set: { plans: newPlans } }
-        );
-        updateRooms(req.body[0]);
-        res.send();
+    });
+
+    app.post("/updateList", (req, res) => {
+        handleErrors(async () => {
+            let group = JSON.parse(req.body);
+            if (JSON.parse(req.body).length > 0)
+                await database.groups.updateOne(
+                    { token: group.token },
+                    { $set: { list: group.list } }
+                );
+            await updateRooms(group.token);
+
+            res.send();
+        });
+    });
+
+    app.post("/api/remove-plan", (req, res) => {
+        handleErrors(async () => {
+            let group = await database.groups.findOne({ token: req.body[0] });
+            let plans = group?.plans;
+            let newPlans: any = [];
+            plans?.forEach((plan) => {
+                if (plan.text != req.body[1].text) newPlans.push(plan);
+            });
+            await database.groups.updateOne(
+                { token: req.body[0] },
+                { $set: { plans: newPlans } }
+            );
+            await updateRooms(req.body[0]);
+            res.send();
+        });
     });
 
     // lights
-    app.post("/get-lights", async (req, res) => {
-        const { lights } =
-            (await database.groups.findOne({ token: req.body })) ?? {};
-        res.send(lights);
+    app.post("/get-lights", (req, res) => {
+        handleErrors(async () => {
+            const { lights } =
+                (await database.groups.findOne({ token: req.body })) ?? {};
+            res.send(lights);
+        });
     });
 
-    app.post("/api/add-light", async (req, res) => {
-        const { token } = req.body;
-        await database.lastId.updateOne({}, { $inc: { id: 1 } });
-        const { id } = (await database.lastId.findOne()) ?? {};
-        if (!id) {
-            throw new Error("No id");
-        }
-        io.send("0|SET|" + id);
-        await database.groups.updateOne(
-            { token },
-            {
-                $push: {
-                    lights: {
-                        id,
-                        name: "new",
-                        work: false,
-                    },
-                },
+    app.post("/api/add-light", (req, res) => {
+        handleErrors(async () => {
+            const { token } = req.body;
+            await database.lastId.updateOne({}, { $inc: { id: 1 } });
+            const { id } = (await database.lastId.findOne()) ?? {};
+            if (!id) {
+                throw new Error("No id");
             }
-        );
-        res.send();
+            io.send("0|SET|" + id);
+            await database.groups.updateOne(
+                { token },
+                {
+                    $push: {
+                        lights: {
+                            id,
+                            name: "new",
+                            work: false,
+                        },
+                    },
+                }
+            );
+            res.send();
+        });
     });
 
-    app.post("/api/switch-light", async (req, res) => {
-        const { id, work, token } = req.body;
-        io.send(id + "|" + (work ? "ON" : "OFF"));
-        await database.groups.updateOne(
-            { token, "lights.id": id },
-            { $set: { "lights.$.work": work } }
-        );
-        res.send();
+    app.post("/api/switch-light", (req, res) => {
+        handleErrors(async () => {
+            const { id, work, token } = req.body;
+            io.send(id + "|" + (work ? "ON" : "OFF"));
+            await database.groups.updateOne(
+                { token, "lights.id": id },
+                { $set: { "lights.$.work": work } }
+            );
+            res.send();
+        });
     });
 
-    app.post("/api/remove-light", async (req, res) => {
-        const { token, id } = req.body;
-        io.send(id + "|SET|0");
-        database.groups.updateOne({ token }, { $pull: { lights: { id } } });
-        res.send();
+    app.post("/api/remove-light", (req, res) => {
+        handleErrors(async () => {
+            const { token, id } = req.body;
+            io.send(id + "|SET|0");
+            await database.groups.updateOne(
+                { token },
+                { $pull: { lights: { id } } }
+            );
+            res.send();
+        });
     });
 
-    app.post("/api/edit-light", async (req, res) => {
-        let { id, name, token } = req.body;
+    app.post("/api/edit-light", (req, res) => {
+        handleErrors(async () => {
+            let { id, name, token } = req.body;
 
-        await database.groups.updateOne(
-            { token, "lights.id": id },
-            { $set: { "lights.$.name": name } }
-        );
-        res.send();
+            await database.groups.updateOne(
+                { token, "lights.id": id },
+                { $set: { "lights.$.name": name } }
+            );
+            res.send();
+        });
     });
 
     server.listen(process.env.PORT || 8080);
@@ -312,7 +359,7 @@ async function main() {
 
         console.log(confirmEmailToken);
 
-        try {
+        await handleErrors(async () => {
             await nodemailer
                 .createTransport({
                     sendmail: true,
@@ -335,11 +382,7 @@ async function main() {
     <a href="https://egondola.eu/confirm/${confirmEmailToken}"><button style="border: none; padding: 10px 30px; border-radius: 5px; color: white; background-color: #546e7a;">Potwierdź adres email</button></a>
 </div>`,
                 });
-        } catch (error) {
-            if (error instanceof Error) {
-                console.error(error?.message);
-            }
-        }
+        });
 
         return "Konto zostało utworzone! Dokończ rejestrację klikając w link wysłany na podany adres email";
     }
@@ -387,5 +430,13 @@ async function main() {
         };
 
         return "";
+    }
+}
+
+async function handleErrors(callback: () => Promise<void>) {
+    try {
+        await callback();
+    } catch (error) {
+        console.error(error instanceof Error ? error.message : error);
     }
 }
